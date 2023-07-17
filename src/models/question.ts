0f89576a63495e3a9da7a578ac0e69eaf180f2e6
lgrante <louisgrante@outlook.fr>
2023-07-17 20:21:34 +0200

    added some tasks

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,18 +1,37 @@
 // @ts-nocheck
-import mongoose, { Schema, Document } from 'mongoose';
-import Question from '../types/question';
+import mongoose, { Schema, Document } from 'mongoose'
+import Question from '../types/question'
 
+// TODO: Fix the model
 
 const QuestionSchema = new Schema<Question>({
-  id: { type: String, required: true },
-  text: { type: String, required: true },
-  responses: [{ type: String, required: true }],
-  nextQuestions: {
-    type: Array,
-    default: undefined,
-  },
-});
+  //id: {
+  //  type: String,
+  //  required: true
+  //},
+  //text: {
+  //  type: String,
+  //  required: true
+  //},
+  //inputType: {
+  //  type: String,
+  //  required: true
+  //},
+  //inputProps: {
+  //  type: Schema.Types.Mixed
+  //},
+  //responses: [
+  //  {
+  //    type: String,
+  //    required: true
+  //  }
+  //],
+  //nextQuestions: {
+  //  type: Array,
+  //  default: undefined,
+  //},
+})
 
-const Question = mongoose.models.Question || mongoose.model('Question', QuestionSchema);
+const Question = mongoose.models.Question || mongoose.model('Question', QuestionSchema)
 
-export default Question;
+export default Question
